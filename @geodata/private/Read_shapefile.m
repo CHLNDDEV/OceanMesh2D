5abdc138a439ce235f40789a6b4de0930d7b7ff7
Keith Roberts <krober10@nd.edu>
2018-07-26 23:05:32 -0400

    Vectorized read_shapefile.m     -Speed up classification of PSLG segment types.

--- @geodata/private/Read_shapefile.m
+++ @geodata/private/Read_shapefile.m
@@ -1,23 +1,25 @@
 function polygon_struct = Read_shapefile( finputname, polygon, bbox, ...
     h0, plot_on )
 % Read_shapefile: Reads a shapefile or a NaN-delimited vector
-% containing polygons and/or segments in the the desired region 
+% containing polygons and/or segments in the the desired region
 % of interest. Classifies the vector data as either a
 % mainland, island, or outer boundary. The program will automatically
 % trim islands that are have an area smaller than 4*h0^2 and will in gaps
 % in the vector that are larger than h0/2. This is necessary for the
-% use of boundary rejection method in dpoly. 
+% use of boundary rejection method in dpoly.
 
-% INPUTS: 
+% INPUTS:
 % finputname : file name(s) of the shapefile listed in a cell array
 % polygon    : a NaN-delimited vector of polygons and/or segments.
 % bbox       : the bounding box that we want to extract out
-% h0         : minimum edge length in region of interest. 
+% h0         : minimum edge length in region of interest.
 % plot_on    : plot the final polygon or not (=1 or =0)
-% OUTPUTS: 
+%
+% OUTPUTS:
 % polygon_struct    : a structure containing the vector features identified as
 %              either islands, mainland, or outer.
 % Written by William Pringle and Keith Roberts, CHL,UND, 2017
+% Edits by Keith Roberts, July 2018. 
 %% Loop over all the filenames and get the shapefile within bbox
 SG = [];
 if (size(finputname,1)~=0)
@@ -38,7 +40,7 @@ if (size(finputname,1)~=0)
             D = S.ncst;
             S = cell2struct(D','points',1);
         end
-        if ~isempty(S) 
+        if ~isempty(S)
             % Keep the following polygons
             SG = [SG; S];
         end
@@ -49,12 +51,10 @@ else
     for i = 1 : length(polygon)
         if(isnan(polygon(i,1))==1)
             count = count + 1; j=1;
-            continue 
+            continue
         end
         SG(count).points(j,:) = polygon(i,:);
-        %SG(count).X(j) = polygon(i,1);
-        %SG(count).Y(j) = polygon(i,2);
-        j=j+1; 
+        j=j+1;
     end
 end
 % If we don't have an outer polygon already then make it by bbox
@@ -64,10 +64,8 @@ polygon_struct.outer = [bbox(1,1) bbox(2,1);
     bbox(1,2) bbox(2,1);
     bbox(1,1) bbox(2,1)];
 % Densify the outer polygon (fills gaps larger than half min edgelength).
-%[latout,lonout] = interpm(polygon_struct.outer(:,2),...
-%    polygon_struct.outer(:,1),h0/2);
 [latout,lonout] = my_interpm(polygon_struct.outer(:,2),...
-     polygon_struct.outer(:,1),h0/2);
+    polygon_struct.outer(:,1),h0/2);
 polygon_struct.outer = [];
 polygon_struct.outer(:,1) = lonout;
 polygon_struct.outer(:,2) = latout;
@@ -79,17 +77,31 @@ polygon_struct.inner = [];
 polygon_struct.mainland = [];
 edges = Get_poly_edges( [polygon_struct.outer; NaN NaN] );
 
-for s = 1:length(SG)
-    if exist('shaperead','file')
-        points = [SG(s).X' SG(s).Y'];
-        points(isnan(points(:,1)),:) = [];
-    else
-        points = SG(s).points;
-    end
-    % Get current polygon
-    % Check proportion of polygon that is within bbox
-    In = inpoly(points,[polygon_struct.outer; NaN NaN], edges);
 
+if exist('shaperead','file')
+    tmpM = [[SG.X]',[SG.Y]'] ; % MAT 
+    for i = 1 : length(SG) 
+       dims(i) = length(SG(i).X) ; 
+    end
+    tmpC = mat2cell(tmpM,dims); % TO CELL 
+else
+    tmpM =  cell2mat(struct2cell(SG)'); 
+    tmpC =  struct2cell(SG)'; 
+end
+% Get current polygon
+% Check proportion of polygon that is within bbox
+tmpIn = inpoly(tmpM,[polygon_struct.outer; NaN NaN], edges);
+tmpInC = mat2cell(tmpIn,cellfun(@length,tmpC));
+
+j = 0 ; k = 0 ; 
+for i = 1 : length(SG)
+    if exist('shaperead','file')
+        points = tmpC{i}(1:end-1,:) ;
+        In     = tmpInC{i}(1:end-1) ;
+    else
+        points = tmpC{i}(1:end,:) ;
+        In     = tmpInC{i}(1:end) ;
+    end
     % lets calculate the area of the
     % feature using the shoelace algorithm and decided whether to keep or
     % not based on area.
@@ -102,15 +114,16 @@ for s = 1:length(SG)
         continue;
     elseif length(find(In == 1)) == length(points)
         % Wholey inside box, set as island (with NaN delimiter)
-        new_island = [points; NaN NaN];
-        polygon_struct.inner = [polygon_struct.inner; new_island];
+        k = k + 1 ; 
+        new_island{k} = [points; NaN NaN];
     else
         % Partially inside box, set as mainland
-        new_main = [points; NaN NaN];
-        polygon_struct.mainland = [polygon_struct.mainland; new_main];
+        j = j + 1 ; 
+        new_main{j} = [points; NaN NaN];
     end
 end
-
+polygon_struct.inner = [polygon_struct.inner; cell2mat(new_island')];
+polygon_struct.mainland = [polygon_struct.mainland; cell2mat(new_main')];
 polygon_struct.outer = [polygon_struct.outer; NaN NaN; polygon_struct.mainland];
 
 %% Plot the map
