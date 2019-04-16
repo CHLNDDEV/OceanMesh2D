f6fc5a47d5ce3798238c583c4c70b4bd0f4eaf2d
William Pringle <wpringle@nd.edu>
2019-04-16 10:26:09 -0400

    changes to utlitlies
    - bug fixes to utilities for size catch and change default smagorisnky in make_f15

--- utilities/renumberEdges.m
+++ utilities/renumberEdges.m
@@ -7,7 +7,7 @@ for i = 1 : length(idx)
 end
 
 % renumber bnde to correspond with pfix
-for i = 1 : length(edges)
+for i = 1 : size(edges,1)
     edges(i,1) = map(edges(i,1));
     edges(i,2) = map(edges(i,2));
 end
