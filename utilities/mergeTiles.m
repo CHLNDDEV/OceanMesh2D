3f4957dbf209822c11778c941ea28b5825ae154f
wpringle@nd.edu <wpringle@nd.edu>
2019-04-18 21:28:17 -0400

    mergeTiles modified to only use plus operator which can now handle unconnected tiles, furthermore implemented a complex loop strategy which is faster than the trivial plus in sequence

--- utilities/mergeTiles.m
+++ utilities/mergeTiles.m
@@ -1,43 +1,34 @@
- function m3 = mergeTiles(tiles) 
-            % Merge mesh tiles together 
-            numTiles = length(tiles) ; 
-            
-            for i = 1 : numTiles-1
-                
-                if i == 1
-                   dmy=load(tiles{i}) ;
-                   m1 = dmy.m ; 
-                else
-                   m1 = m3 ;
-                end
-                dmy=load(tiles{i+1}) ; 
-                m2 = dmy.m ; 
-
-                
-                dt = delaunayTriangulation(m1.p) ;
-                dt.Points(end+(1:length(m2)),:) = m2.p ;
-                
-                m3 = msh() ;
-                m3.p = dt.Points ;
-                m3.t = dt.ConnectivityList ;
-                
-                bc = baryc(m3) ;
-                
-                bou1 = m1.getBoundaryOfMesh ;
-                ee1   = Get_poly_edges(bou1) ;
-                in1  = inpoly(bc,bou1,ee1) ;
-                
-                bou2 = m2.getBoundaryOfMesh ;
-                ee2   = Get_poly_edges(bou2) ;
-                in2  = inpoly(bc,bou2,ee2) ;
-                
-                rm = ~in1 & ~in2 ;
-                
-                m3.t(find(rm),:) = [ ] ;
-                
-                [m3.p,m3.t]=fixmesh(m3.p,m3.t) ;
-                
-                m3 = renum(m3) ;
-            end
-            
+function merge = mergeTiles(tiles) 
+    % Merge mesh tiles together 
+    % complex looping strategy is faster than the trivial addition because
+    % it has fewer additions with a large mesh
+    % note that plus can now handle connected and non-connected tiles
+    
+    numTiles = length(tiles);
+    n = 0;
+    for i = 1: 2 : numTiles
+        dmy = load(tiles{i}) ;
+        n = n + 1;
+        disp(['merge #' num2str(n)])
+        merge{n} = dmy.m;
+        if i + 1 <= numTiles
+            dmy = load(tiles{i+1}) ;
+            merge{n} = dmy.m + merge{n};
         end
+    end
+    
+    while length(merge) > 1
+        n = 0;
+        for i = 1 : 2 : length(merge)
+            n = n + 1;
+            disp(['merge #' num2str(n)])
+            if i + 1 <= length(merge) 
+                merge{n} = merge{i+1} + merge{i};
+            else
+                merge{n} = merge{i}; 
+            end
+        end 
+        merge(n+1:end) = [];
+    end
+    merge = merge{1};
+end
