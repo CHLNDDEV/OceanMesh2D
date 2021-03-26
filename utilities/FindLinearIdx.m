945101fcdc7b1b38497d4ed5969d00d883b74051
William Pringle <wpringle@anl.gov>
2021-03-25 23:09:01 -0500

    Testing for irregular DEM grid in FindLinearIdx function in order to interpolate properly using cell-averaged method for irregular DEM grid

--- utilities/FindLinearIdx.m
+++ utilities/FindLinearIdx.m
@@ -4,32 +4,55 @@ function [IX,IX1,IX2] = FindLinearIdx(x,y,lon,lat)
 % lon and lat must be matrices created by ndgrid. 
 % kjr, 20171210 chl, und.
 % wjp, 20201120 making sure dx and dy can be different
+%      20200325 implementing method for irregular grid, cleaning up
 ny = size(lon,1);
 nx = size(lon,2);
 np = numel(x);
 
-X = reshape(lon,[],1);
-Y = reshape(lat,[],1);
-
-% Get the grid spacing in x and y directions 
-% (trying both directions so could be meshgrid or ndgrid format)
-dx  = max(abs(lon(2,1)-lon(1,1)),abs(lon(1,2)-lon(1,1)));
-dy  = max(abs(lat(1,2)-lat(1,1)),abs(lat(2,1)-lat(1,1)));
-
+% make sure entry points are column vectors
 x = x(:);
 y = y(:);
 
-IX1 = (x-X(1))./dx + 1;
-IX2 = (y-Y(1))./dy + 1;
+% Get the grid spacing in x and y directions 
+% (trying both directions so could be meshgrid or ndgrid format)
+dx  = diff(lon(:,1));
+dy  = diff(lat(1,:));
 
-IX1 = round(IX1);
-IX2 = round(IX2);
+if max(dx) ~= min(dx)
+    % % IRREGULAR GRID (SLOWER)
+    
+    % convert ndgrid to vector
+    lonV = reshape(lon(:,1),1,[]); 
+    % repeat the vector along the length of x
+    LON = repmat(lonV,np,1);
+    % find the closest lon to each point of x
+    [~,IX1] = min(abs(x - LON),[],2);
+    
+    % convert ndgrid to vector
+    latV = lat(1,:); 
+    % repeat the vector along the length of y
+    LAT = repmat(latV,np,1);
+    % find the closest lon to each point of y
+    [~,IX2] = min(abs(y - LAT),[],2);
 
-IX1 = max([IX1,ones(np,1)],[],2);
-IX1 = min([IX1,ny*ones(np,1)],[],2);
-%
-IX2 = max([IX2,ones(np,1)],[],2);
-IX2 = min([IX2,nx*ones(np,1)],[],2);
+else
+    % % REGULAR GRID (FASTER)
+    dx = dx(1); dy = dy(1);
+
+    IX1 = (x-lon(1,1))/dx + 1;
+    IX2 = (y-lat(1,1))/dy + 1;
+
+    IX1 = round(IX1);
+    IX2 = round(IX2);
+
+    IX1 = max([IX1,ones(np,1)],[],2);
+    IX1 = min([IX1,ny*ones(np,1)],[],2);
+
+    IX2 = max([IX2,ones(np,1)],[],2);
+    IX2 = min([IX2,nx*ones(np,1)],[],2);
+
+end
 
 IX = sub2ind([ny,nx],IX1,IX2);
+
 end
