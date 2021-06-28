8378a39f453bee4628cc1720fc8f9746c5155847
Keith Roberts <krober@usp.edu>
2021-06-28 12:36:52 -0300

    updating format

--- @msh/private/writefort24.m
+++ @msh/private/writefort24.m
@@ -1,4 +1,4 @@
-function fid = writefort24( f24dat, finame, format )
+function fid = writefort24( f24dat, finame, points, format )
 %
 %
 if ( nargin == 1 )
@@ -40,33 +40,46 @@ elseif strcmp(format,'netcdf') % NETCDF
     end
     % deflate level (set zero for no deflation if necessary.. using single precision so not very different in size)
     dl = 5;
-    % SAL indices
-    indices = squeeze(f24dat.Val(1,1,:)); 
-    sal_nodes = length(indices);
-    nccreate(file,'mesh_indices','Dimensions',{'sal_nodes',sal_nodes},'DataType','int32','DeflateLevel',dl) ;
-    ncwriteatt(file,'mesh_indices','long_name','index of SAL values into mesh coordinates')
-    ncwrite(file, 'mesh_indices', indices) ;
-    % SAL name
+    
+    node = length(points); 
     tipnames = char(f24dat.tiponame); ntip = size(tipnames) ;
-    nccreate(file,'const','dimensions',{'num_const',ntip(1),'char_len',ntip(2)},'DataType','char') ;
-    ncwriteatt(file,'const','long_name','names of the tidal harmonic constituents')
-    ncwrite(file, 'const', tipnames) ;
-    % SAL frequencies
-    nccreate(file,'frequency','dimensions',{'num_const',ntip(1)});
-    ncwriteatt(file,'frequency','long_name','frequency of harmonic constituents')
-    ncwriteatt(file,'frequency','units','rad/s')
+    fillvalue = 0.0;
+        
+    nccreate(file,'x','Dimensions',{'node',node},'DataType','int32','DeflateLevel',dl) ;
+    ncwriteatt(file,'x','standard_name', 'longitude') ;
+    ncwriteatt(file,'x','units', 'degrees_east') ;
+    ncwriteatt(file,'x','positive', 'east') ;
+    ncwrite(file, 'x', points(:,1)) ;
+
+    nccreate(file,'y','Dimensions',{'node',node},'DataType','int32','DeflateLevel',dl) ;
+    ncwriteatt(file,'y','standard_name', 'latitude') ;
+    ncwriteatt(file,'y','units', 'degrees_north') ;
+    ncwriteatt(file,'y','positive', 'north') ;
+    ncwrite(file, 'y', points(:,2)) ;    
+    
+    nccreate(file,'constituents','Dimensions',{'num_constituents',ntip(1),'char_len',ntip(2)},'DataType','char') ;
+    ncwriteatt(file,'constituents','standard_name', 'name_of_tidal_harmonic_constituents') ;
+    ncwriteatt(file,'constituents','long_name', 'name of tidal harmonic constituents') ;
+    ncwrite(file, 'constituents', tipnames) ;
+    
+    nccreate(file,'frequency','dimensions',{'num_constituents',ntip(1)});
+    ncwriteatt(file,'frequency','standard_name','frequency_of_tidal_harmonic_constituents')
+    ncwriteatt(file,'frequency','long_name','frequency of tidal harmonic constituents')
+    ncwriteatt(file,'frequency','units','radians/second')
     ncwrite(file, 'frequency', f24dat.omega) ;
-    % SAL amplitudes
-    nccreate(file, 'SAL_amp','Dimensions',{'num_const',ntip(1),'sal_nodes',sal_nodes},'DataType','single','DeflateLevel',dl) ;
-    ncwriteatt(file,'SAL_amp','long_name','amplitude of self-attraction and loading tide elevation')
-    ncwriteatt(file,'SAL_amp','units','m')
-    ncwrite(file, 'SAL_amp', squeeze(f24dat.Val(:,2,:))) ;
-    % SAL phase lags
-    nccreate(file, 'SAL_phs','Dimensions',{'num_const',ntip(1),'sal_nodes',sal_nodes},'DataType','single','DeflateLevel',dl) ;
-    ncwriteatt(file,'SAL_phs','long_name','phase-lag of self-attraction and loading tide elevation')
-    ncwriteatt(file,'SAL_phs','units','degrees with respect to GMT/UTC')
-    ncwrite(file, 'SAL_phs', squeeze(f24dat.Val(:,3,:))) ;
-    % Add some meta data/attribution
+    
+    nccreate(file, 'sal_amplitude','Dimensions',{'num_constituents',ntip(1),'node',node},'DataType','single','DeflateLevel',dl,'FillValue',fillvalue) ;
+    ncwriteatt(file,'sal_amplitude','standard_name','amplitude_of_self_attraction_and_loading_tide_elevation')
+    ncwriteatt(file,'sal_amplitude','long_name','amplitude of self attraction and loading tide elevation')
+    ncwriteatt(file,'sal_amplitude','units','m')
+    ncwrite(file, 'sal_amplitude', squeeze(f24dat.Val(:,2,:))) ;
+    
+    nccreate(file, 'sal_phase','Dimensions',{'num_constituents',ntip(1),'node',node},'DataType','single','DeflateLevel',dl,'FillValue',fillvalue) ;
+    ncwriteatt(file,'sal_phase','long_name','phase-lag of self-attraction and loading tide elevation')
+    ncwriteatt(file,'sal_phase','standard_name','phase_lag_of_self_attraction_and_loading_tide_elevation')
+    ncwriteatt(file,'sal_phase','units','degrees with respect to GMT/UTC')
+    ncwrite(file, 'sal_phase', squeeze(f24dat.Val(:,3,:))) ;
+   
     ncwriteatt(file,'/','title','The self-attraction and loading terms for an ADCIRC simulation');
     ncwriteatt(file,'/','creation_date',datestr(now));
     ncwriteatt(file,'/','source',"Made by OceanMesh2D writefort24");
