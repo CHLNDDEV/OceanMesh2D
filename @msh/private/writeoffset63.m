ffd711c2c12f382bef975fce9a1edae8b0be9716
William Pringle <wpringle@anl.gov>
2023-06-09 10:04:09 -0500

    add catch for writing out offset when offset_nodes are int type (otherwise it converted the offset_values to integer)

--- @msh/private/writeoffset63.m
+++ @msh/private/writeoffset63.m
@@ -12,8 +12,12 @@ fprintf(fid,'%s \n',f63dat.header) ;
 fprintf(fid,'%12.6f \n',f63dat.time_interval) ;
 fprintf(fid,'%12.6f \n',f63dat.default_val) ;
 
+% if offset_nodes int type this causes problems when combined with offset_values
+offset_nodes = single(f63dat.offset_nodes);
+
 for tt = 1: f63dat.num_times
-    fprintf(fid,'%d \t %12.6f \n',[f63dat.offset_nodes; f63dat.offset_values(tt,:)]);
+    fprintf(fid,'%d \t %12.6f \n',...
+        [offset_nodes; f63dat.offset_values(tt,:)]);
     fprintf(fid,'%s \n','##'); 
 end
   
