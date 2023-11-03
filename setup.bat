af199547216d15686feaec2d11337c8d5a2135d7
omouraenko <mouraenko@gmail.com>
2023-11-03 11:39:30 -0500

    Updated 'setup.bat' and 'setup.sh' scripts * Added option to keep zip file * Added more reporting * Replaced 'powershell' with 'tar' in batch file (tar is available in Windows 10)

--- setup.bat
+++ setup.bat
@@ -6,10 +6,11 @@
 :: #                                                  #
 :: # Requires wget and unzip                          #
 :: ####################################################
-:: # Can toggle parameters if desired but the script
-:: # checks for their existance too
 
+:: Can toggle parameters if desired but the script
+:: checks for their existence too
 :: set to 'true' to download
+
 :: m_map mapping toolbox (reqd)
 set m_map=true
 :: global shoreline
@@ -19,48 +20,64 @@ set srtm=true
 :: GEBCO_2020 global bathymetry  -> {desired source}
 set gebco=false
 
+:: keep downloaded zip files
+set keepzip=false
+
 :: download m_map archive and unzip
 if "%m_map%" == "true" (
-  if EXIST "m_map" (
-    echo m_map directory already exists
-  ) else (
-    wget --no-check-certificate "https://www.eoas.ubc.ca/~rich/m_map1.4.zip" -O m_map.zip
-    powershell -command "Expand-Archive -Force 'm_map.zip' -DestinationPath ."
-    del m_map.zip > nul
-  )
+    if EXIST "m_map" (
+        echo "m_map" directory already exists
+    ) else (
+        echo Setting up m_map
+        if NOT EXIST "m_map.zip" wget --no-check-certificate --no-hsts "https://www.eoas.ubc.ca/~rich/m_map1.4.zip" -O m_map.zip
+        tar -xf m_map.zip 
+        if NOT "%keepzip%" == "true" del m_map.zip > nul
+    )
+) else (
+    echo Skipping m_map
 )
 
 :: move to datasets directory
 if NOT EXIST "datasets" mkdir datasets
+
 pushd datasets
 
 :: download GSHHS_shp shoreline
 if "%gshhs%" == "true" (
-  if EXIST "GSHHS_shp" (
-    echo GSHHS_shp directory ^(global shoreline^) already exists
-  ) else (
-    wget --no-check-certificate "http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-shp-2.3.7.zip" -O gshhs.zip
-    powershell -command "Expand-Archive -Force 'gshhs.zip' -DestinationPath ."
-    del gshhs.zip > nul
-  )
+    if EXIST "GSHHS_shp" (
+        echo "datasets/GSHHS_shp" directory ^(global shoreline^) already exists
+    ) else (
+        echo Setting up GSHHS shoreline
+        if NOT EXIST "gshhs.zip" wget --no-check-certificate --no-hsts "http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-shp-2.3.7.zip" -O gshhs.zip
+        tar -xf gshhs.zip GSHHS_shp
+        if NOT "%keepzip%" == "true" del gshhs.zip > nul
+    )
+) else (
+    echo Skipping GSHHS shoreline
 )
 
 :: download SRTM15+ bathymetry
 if "%srtm%" == "true" (
-  if EXIST "SRTM15+.nc" (
-    echo SRTM15+.nc global bathymetry file already exists
-  ) else (
-    wget --no-check-certificate "https://topex.ucsd.edu/pub/srtm15_plus/SRTM15_V2.4.nc" -O SRTM15+.nc
-  )
+    if EXIST "SRTM15+.nc" (
+        echo "datasets/SRTM15+.nc" global bathymetry file already exists
+    ) else (
+        echo Setting up SRTM15+ bathymetry
+        wget --no-check-certificate --no-hsts "https://topex.ucsd.edu/pub/srtm15_plus/SRTM15_V2.4.nc" -O SRTM15+.nc
+    )
+) else (
+    echo Skipping SRTM15+ bathymetry
 )
 
 :: download GEBCO_2020.nc bathymetry
 if "%gebco%" == "true" (
-  if EXIST "GEBCO_2020.nc" (
-    echo "GEBCO_2020.nc global bathymetry file already exists"
-  ) else (
-    wget --no-check-certificate "https://www.bodc.ac.uk/data/open_download/gebco/gebco_2021/zip/" -O GEBCO_2020.nc
-  )
+    if EXIST "GEBCO_2020.nc" (
+        echo "datasets/GEBCO_2020.nc" global bathymetry file already exists
+    ) else (
+        echo Setting up GEBCO_2020 bathymetry
+        wget --no-check-certificate --no-hsts "https://www.bodc.ac.uk/data/open_download/gebco/gebco_2021/zip/" -O GEBCO_2020.nc
+    )
+) else (
+    echo Skipping GEBCO_2020 bathymetry
 )
 
 popd 
--- setup.sh
+++ setup.sh
@@ -5,22 +5,32 @@
 #                                                  #
 # Requires wget and unzip                          #
 ####################################################
+
 # Can toggle parameters if desired but the script
 # checks for their existance too
+# set to 'true' to download
+
 m_map=true  # m_map mapping toolbox (reqd)
 gshhs=true  # global shoreline
 srtm=true   # SRTM15+V2.1 global bathymetry -> {user can select
 gebco=false  # GEBCO_2020 global bathymetry  -> {desired source
 
+keepzip=false # keep downloaded zip files
+
+# download m_map archive and unzip
 if $m_map; then
-  if [ -d "m_map" ]; then
-     echo "m_map directory already exists"
-  else
-     # download m_map archive and unzip
-     wget --no-check-certificate "https://www.eoas.ubc.ca/~rich/m_map1.4.zip" -O m_map.zip
-     unzip m_map.zip
-     rm m_map.zip
-  fi
+    if [ -d "m_map" ]; then
+        echo "\"m_map\" directory already exists"
+    else
+        echo "Setting up m_map"
+        if [ ! -f "m_map.zip" ]; then 
+            wget --no-check-certificate --no-hsts "https://www.eoas.ubc.ca/~rich/m_map1.4.zip" -O m_map.zip
+        fi
+        unzip -q m_map.zip
+        if ! $keepzip; then rm m_map.zip; fi
+    fi
+else
+    echo "Skipping m_map setup"
 fi
 
 # move to datasets directory
@@ -30,33 +40,44 @@ fi
  
 cd datasets/
 
+# download m_map archive and unzip
 if $gshhs; then
-  if [ -d "GSHHS_shp" ]; then
-     echo "GSHHS_shp directory (global shoreline) already exists"
-  else
-     # download m_map archive and unzip
-     wget --no-check-certificate "http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-shp-2.3.7.zip" -O gshhs.zip
-     unzip gshhs.zip "GSHHS_shp/*"
-     rm gshhs.zip
-  fi
+    if [ -d "GSHHS_shp" ]; then
+        echo "\"datasets/GSHHS_shp\" directory (global shoreline) already exists"
+    else
+        echo "Setting up GSHHS shoreline"
+        if [ ! -f "gshhs.zip" ]; then 
+            wget --no-check-certificate --no-hsts "http://www.soest.hawaii.edu/pwessel/gshhg/gshhg-shp-2.3.7.zip" -O gshhs.zip
+        fi
+        unzip -q gshhs.zip "GSHHS_shp/*"
+        if ! $keepzip; then rm gshhs.zip; fi
+    fi
+else
+    echo "Skipping m_map"
 fi
 
+# download SRTM15+ bathymetry
 if $srtm; then
-  if [ -f "SRTM15+.nc" ]; then
-     echo "SRTM15+.nc global bathymetry file already exists"
-  else
-     # download SRTM15+ bathymetry
-     wget --no-check-certificate "https://topex.ucsd.edu/pub/srtm15_plus/SRTM15_V2.4.nc" -O SRTM15+.nc
-  fi
+    if [ -f "SRTM15+.nc" ]; then
+        echo "\"datasets/SRTM15+.nc\" global bathymetry file already exists"
+    else
+        echo "Setting up SRTM15+ bathymetry"
+        wget --no-check-certificate --no-hsts "https://topex.ucsd.edu/pub/srtm15_plus/SRTM15_V2.4.nc" -O SRTM15+.nc
+    fi
+else
+    echo "Skipping SRTM15+ bathymetry"
 fi
 
+# download GEBCO_2020.nc bathymetry
 if $gebco; then
-  if [ -f "GEBCO_2020.nc" ]; then
-     echo "GEBCO_2020.nc global bathymetry file already exists"
-  else
-     # download GEBCO_2020.nc bathymetry
-     wget --no-check-certificate "https://www.bodc.ac.uk/data/open_download/gebco/gebco_2021/zip/" -O GEBCO_2020.nc
-  fi
+    if [ -f "GEBCO_2020.nc" ]; then
+        echo "\"datasets/GEBCO_2020.nc\" global bathymetry file already exists"
+    else
+        echo "Setting up GEBCO_2020 bathymetry"
+        wget --no-check-certificate --no-hsts "https://www.bodc.ac.uk/data/open_download/gebco/gebco_2021/zip/" -O GEBCO_2020.nc
+    fi
+else
+    echo "Skipping GEBCO_2020 bathymetry"
 fi
 
-read -p "Press any key to resume ..."
+read -p "Press [ENTER] to continue ..." answer
